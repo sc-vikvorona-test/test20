dab213ba2ae4c8e4f750af433fa1d458a182cbb5
Viktor Vorona <viktor.vorona@sonarsource.com>
2025-08-26 15:49:31 +0200

    t1

--- src/test-lol.js
+++ src/test-lol.js
@@ -6,7 +6,22 @@ const iv = "12345";
 location.href = "javascript:void(0)"; // Sensitive
 location.href = "javascript:void(0)"; // Sensitive
 
+const lol = (test, test2) => {
+    let pointer = 0;
+    const instructions = test.split(",").map((x) => parseInt(x, 10));
+    let A = BigInt(test2);
+    let B = 0n;
+    let C = 0n;
 
+    while (pointer < instructions.length) {
+        const operator = instructions[pointer];
+        const operand = instructions[pointer + 1];
+        pointer = operate(operator, operand, pointer);
+    }
+    return res.map((x) => x.toString(16)).join("");
+}
+
+lol('1', 2)
 
 // AZBURwZL83ztCUnc4EG6
 // AZBURwZL83ztCUnc4EG51
