9646781b5c7efb17df897536b49e52dbac1c49d5
Viktor Vorona <viktor.vorona@sonarsource.com>
2025-08-27 17:25:12 +0200

    l

--- src/test-lol.js
+++ src/test-lol.js
@@ -18,6 +18,16 @@ const lol = (test, test2, test3, test4, test5, test6, test7, test8, test9) => {
         const operand = 2;
         pointer = operate(pointer);
     }
+
+    while (pointer < 2) {
+        const a = 1;
+        const b = 2;
+        const c = 2;
+        const d = 2;
+        const e = 2;
+        const f = 2;
+        pointer = operate(pointer, b,c,d,e,f);
+    }
     return res.map((x) => x.toString(16)).join("");
 }
 
