212770e3e43dee6cc584e17f7a3ba3b0fa118909
Viktor Vorona <viktor.vorona@sonarsource.com>
2025-09-09 17:20:07 +0200

    LOL

--- src/test-lol.js
+++ src/test-lol.js
@@ -19,8 +19,23 @@ location.href = "javascript:void(0)"; // Sensitive
 location.href = "javascript:void(0)"; // Sensitive
 location.href = "javascript:void(0)"; // Sensitive
 
+
+lol('1', 2)
+
+// AZBURwZL83ztCUnc4EG6
+// AZBURwZL83ztCUnc4EG51
+
 const lol = (test, test2, test3, test4, test5, test6, test7, test8, test9) => {
     let pointer = 0;
+    while (pointer < 2) {
+        const a1 = 1;
+        const a2 = 2;
+        const a3 = 2;
+        const a4 = 2;
+        const a5 = 2;
+        const a6 = 2;
+        pointer = operate(pointer,a1,a2,a3,a4,a5,a6);
+    }
     const instructions = test.split(",").map((x) => parseInt(x, 10));
     const evaluate = test2.toString()
     const evaluate2 = test3.toString()
@@ -32,23 +47,7 @@ const lol = (test, test2, test3, test4, test5, test6, test7, test8, test9) => {
         const d = 2;
         const e = 2;
         const f = 2;
-        pointer = operate(pointer);
-    }
-
-    while (pointer < 2) {
-        const a = 1;
-        const b = 2;
-        const c = 2;
-        const d = 2;
-        const e = 2;
-        const f = 2;
         pointer = operate(pointer,a,b,c,d,e);
     }
-    return res.map((x) => x.toString(16)).join("");
+    return res.map((x) => x.toString(16)).join("" + instructions + evaluate2 + evaluate);
 }
-
-
-lol('1', 2)
-
-// AZBURwZL83ztCUnc4EG6
-// AZBURwZL83ztCUnc4EG51
