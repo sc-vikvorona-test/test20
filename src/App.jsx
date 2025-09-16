0f4642800d19fef8f5f121d2a3f95bcd2844983d
Viktor Vorona <viktor.vorona@sonarsource.com>
2025-09-16 17:43:57 +0200

    123

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,9 @@ function App() {
         >
           Learn React
         </a>
+        <div dangerouslySetInnerHTML={{ __html: "HTML" }}>
+          <div>Children</div>
+        </div>
       </header>
     </div>
   );
