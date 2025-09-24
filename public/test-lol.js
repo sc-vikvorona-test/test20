72d6ea36fa1380fda78c8f8bfe635becfba323fb
Viktor Vorona <viktor.vorona@sonarsource.com>
2025-09-24 16:34:27 +0200

    test

--- public/test-lol.js
+++ public/test-lol.js
@@ -19,6 +19,8 @@ location.href = "javascript:void(0)"; // Sensitive
 location.href = "javascript:void(0)"; // Sensitive
 location.href = "javascript:void(0)"; // Sensitive
 
+crypto.createCipheriv("DES", key, iv); // Noncompliant
+
 
 lol('1', 2)
 
