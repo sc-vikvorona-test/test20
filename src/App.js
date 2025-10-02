fdf78962a729c1c5652253919dbee86e58c4d062
Viktor Vorona <viktor.vorona@sonarsource.com>
2025-10-02 17:20:58 +0200

    test

--- src/App.js
+++ src/App.js
@@ -9,14 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload. Test
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        DO not learn
       </header>
     </div>
   );
